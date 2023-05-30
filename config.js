534e7e1ab4980e204a7ec153d291c1064dd78cf3
khalilcodes <khalilali.j@gmail.com>
2023-05-30 19:08:35 +0300

    [script,#2][s]: add custom header and footer support

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  // specify path to custom header and footer files
+  header: "diginomics-101/header.md",
+  footer: "diginomics-101/footer.md"
+}
--- index.js
+++ index.js
@@ -13,25 +13,45 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkParseFrontmatter from "remark-parse-frontmatter";
 // Brevo (sendinblue) sdk
 import SibApiV3Sdk from "sib-api-v3-sdk";
+import options from "./config.js"
 
 (async () => {
   const markdownFile = await input({
     message: "Enter path to markdown file:"
   })
 
+  let header = "", footer = ""
+
+  if (options.header) {
+    const headerContent = fs.readFileSync(options.header, "utf-8", (err,_) => {
+      if (err) throw new Error(err)
+    })
+    const { value } = await remark().use(remarkHtml, { sanitize: false }).process(headerContent)
+    header = value
+  }
+
+  if (options.footer) {
+    const footerContent = fs.readFileSync(options.footer, "utf-8", (err,_) => {
+      if (err) throw new Error(err)
+    })
+    const { value } = await remark().use(remarkHtml, { sanitize: false }).process(footerContent)
+    footer = value
+  }
+
   const content = fs.readFileSync(markdownFile, "utf-8", (err,_) => {
     if (err) throw new Error("Unable to read file")
   });
   
-  const { data: { frontmatter }, value } = remark()
+  const { data: { frontmatter }, value: body } = remark()
     .use(remarkFrontmatter)
     .use(remarkParseFrontmatter)
     .use(remarkGfm)
-    .use(remarkHtml)
+    .use(remarkHtml, { sanitize: false })
     .processSync(content)
 
   const templateId = frontmatter?.id
-  const html = `<!DOCTTYPE html><html><body>${value}</body></html>`
+  const html = `<!DOCTTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="format-detection" content="telephone=no"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>${frontmatter.title ?? ""}</title><style type="text/css">a, a:link { color: #343434; text-decoration: underline; word-break: break-word; }</style>
+<!--[if mso]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]--></head><body>${header}${body}${footer}</body></html>`
 
   let defaultClient = SibApiV3Sdk.ApiClient.instance;
 
@@ -75,7 +95,6 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
     smtpTemplate.replyTo = frontmatter?.replyTo ?? '[DEFAULT_REPLY_TO]';
   }
   
-  smtpTemplate.toField = '{FNAME}'
   smtpTemplate.htmlContent = html;
   smtpTemplate.isActive = true;
 
@@ -87,6 +106,14 @@ import SibApiV3Sdk from "sib-api-v3-sdk";
     });
   } else {
     await apiInstance.createSmtpTemplate(smtpTemplate).then(function(data) {
+      // Add the template id to frontmatter
+      let updatedContent
+      if (frontmatter) {
+        updatedContent = content.replace(/^(.*)$/m, `---\nid: ${data.id}`)
+      } else {
+        updatedContent = `---\nid: ${data.id}\n---\n\n${content}`
+      }
+      fs.writeFileSync(markdownFile, updatedContent, "utf-8")
       console.log('Template created successfully with templateId: ' + data.id);
     }, function(error) {
       console.error({status: error.response.status, ...error.response.body});
